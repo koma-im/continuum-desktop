c91b30f454bf3e1080d9045e5cffc3e76eb02d22
beatpm <32093840+beatpm@users.noreply.github.com>
2017-11-04 00:41:35 +0400

    remove boilerplates

--- src/koma/gui/view/chatView.kt
+++ src/koma/gui/view/chatView.kt
@@ -3,18 +3,18 @@ package koma.gui.view
 import controller.guiEvents
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon
 import de.jensd.fx.glyphs.fontawesome.utils.FontAwesomeIconFactory
-import javafx.collections.ObservableList
 import javafx.scene.control.Alert
 import javafx.scene.control.ButtonBar
 import javafx.scene.control.TextField
 import javafx.scene.layout.Priority
-import koma.gui.view.messagesview.MessagesScrollListView
+import koma.gui.view.messagesview.fragment.MessageCell
 import koma.gui.view.messagesview.fragment.create_message_cell
 import koma.input.emoji.EmojiPanel
-import koma_app.appState
 import koma.matrix.event.room_message.RoomMessage
+import koma_app.appState
 import model.Room
 import org.fxmisc.flowless.VirtualFlow
+import org.fxmisc.flowless.VirtualizedScrollPane
 import rx.javafx.kt.actionEvents
 import rx.javafx.kt.addTo
 import tornadofx.*
@@ -22,14 +22,22 @@ import tornadofx.*
 class ChatRecvSendView(room: Room): View() {
     override val root = vbox(10.0)
 
-    val messageListView = MessagesScrollListView(room.messageManager.messages)
     private val messageInput = MessageInputView()
 
     init {
         with(root) {
             hgrow = Priority.ALWAYS
-            //+messageListView
-            add(messageListView.root)
+
+            val msgList = room.messageManager.messages
+            val virtualList =  VirtualFlow.createVertical(
+                    msgList, {create_message_cell(it)}, VirtualFlow.Gravity.REAR
+            )
+            virtualList.vgrow = Priority.ALWAYS
+            virtualList.hgrow = Priority.ALWAYS
+            val virtualizedScrollPane = VirtualizedScrollPane<VirtualFlow<RoomMessage, MessageCell>>(virtualList)
+            virtualizedScrollPane.vgrow = Priority.ALWAYS
+            add(virtualizedScrollPane)
+
             add(createButtonBar(messageInput.root))
 
             add(messageInput)
@@ -78,19 +86,6 @@ class MessageInputView(): View() {
                 }.filter{ it.isNotBlank()
                 }.addTo(guiEvents.sendMessageRequests)
 
-        }
-    }
-}
-
-class MessageListView(msgList: ObservableList<RoomMessage>): View() {
-    override val root = VirtualFlow.createVertical(
-            msgList, {create_message_cell(it)}, VirtualFlow.Gravity.REAR
-    )
-
-    init {
-        with(root) {
-            vgrow = Priority.ALWAYS
-            hgrow = Priority.ALWAYS
         }
     }
 }
