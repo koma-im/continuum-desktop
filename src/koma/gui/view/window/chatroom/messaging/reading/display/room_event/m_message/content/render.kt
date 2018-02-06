071b70cbe8aa8648e997a450da7f9f0db2ae1187
trapchar <trapchar@protonmail.com>
2018-02-06 15:41:18 +0400

    notice message display

--- src/koma/gui/view/window/chatroom/messaging/reading/display/room_event/m_message/content/render.kt
+++ src/koma/gui/view/window/chatroom/messaging/reading/display/room_event/m_message/content/render.kt
@@ -2,15 +2,13 @@ package koma.gui.view.window.chatroom.messaging.reading.display.room_event.m_mes
 
 import koma.gui.view.window.chatroom.messaging.reading.display.ViewNode
 import koma.matrix.event.room_message.MRoomMessage
-import koma.matrix.event.room_message.chat.EmoteMessage
-import koma.matrix.event.room_message.chat.FileMessage
-import koma.matrix.event.room_message.chat.ImageMessage
-import koma.matrix.event.room_message.chat.TextMessage
+import koma.matrix.event.room_message.chat.*
 
 fun MRoomMessage.render_node(): ViewNode? {
     val content = this.content
     return when(content) {
         is TextMessage -> MTextViewNode(content)
+        is NoticeMessage -> MNoticeViewNode(content)
         is EmoteMessage -> MEmoteViewNode(content, this)
         is ImageMessage -> MImageViewNode(content)
         is FileMessage -> MFileViewNode(content)
