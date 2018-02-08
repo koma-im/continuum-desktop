f0f70cd8b6266ae7ebd346d897b76af1d3750388
trapchar <trapchar@protonmail.com>
2018-02-09 01:05:20 +0400

    improve preview

--- src/koma/gui/view/window/chatroom/messaging/reading/display/room_event/m_message/common/image.kt
+++ src/koma/gui/view/window/chatroom/messaging/reading/display/room_event/m_message/common/image.kt
@@ -1,11 +1,12 @@
 package koma.gui.view.window.chatroom.messaging.reading.display.room_event.m_message.common
 
 import javafx.scene.control.MenuItem
+import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import javafx.scene.layout.StackPane
 import koma.gui.dialog.file.save.downloadFileAs
 import koma.gui.view.window.chatroom.messaging.reading.display.ViewNode
-import koma.network.media.downloadImageHttp
+import koma.network.media.getResponse
 import koma.storage.config.settings.AppSettings
 import kotlinx.coroutines.experimental.launch
 import okhttp3.HttpUrl
@@ -18,16 +19,16 @@ class ImageElement(val url: HttpUrl): ViewNode {
     init {
         val imageView = ImageView()
         node.add(imageView)
-        imageView.isPreserveRatio = true
+
         val scale = AppSettings.settings.scaling
-        imageView.fitWidth = 200.0 * scale
-        imageView.fitHeight = 200.0 * scale
-        imageView.isSmooth = true
+        val imageSize = 200.0 * scale
 
         menuItems = menuItems()
 
         launch {
-            val image = downloadImageHttp(url)
+            val res = getResponse(url) ?: return@launch
+            val image = Image(res.byteStream(), imageSize, imageSize, true, true)
+            image.widthProperty()
             imageView.image = image
         }
     }
