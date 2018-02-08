54f4eb591410f4d5ac93fe15ace92d7805444dba
trapchar <trapchar@protonmail.com>
2018-02-08 23:16:23 +0400

    fix avatar

--- src/koma/gui/element/icon/AvatarAlways.kt
+++ src/koma/gui/element/icon/AvatarAlways.kt
@@ -1,9 +1,10 @@
 package koma.gui.element.icon
 
 import javafx.beans.value.ObservableValue
+import javafx.scene.image.ImageView
 import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
-import koma.gui.element.icon.avatar.AvatarView
+import koma.gui.element.icon.avatar.AvatarProvider
 import koma.gui.element.icon.placeholder.AvatarPlaceholder
 import koma.storage.config.settings.AppSettings
 import tornadofx.*
@@ -11,13 +12,13 @@ import tornadofx.*
 
 val avatarSize = AppSettings.scaling * 32.0
 
-class AvatarAlways private constructor(ai: AvatarView, ap: AvatarPlaceholder): StackPane() {
+class AvatarAlways private constructor(urlV: ObservableValue<String>, ap: AvatarPlaceholder): StackPane() {
     constructor(
             urlV: ObservableValue<String>,
             nameV: ObservableValue<String>,
             color: Color
     ): this(
-            AvatarView(urlV),
+            urlV,
             AvatarPlaceholder(nameV, color))
 
     constructor(
@@ -25,15 +26,36 @@ class AvatarAlways private constructor(ai: AvatarView, ap: AvatarPlaceholder): S
             nameV: ObservableValue<String>,
             colorV: ObservableValue<Color>
     ): this(
-            AvatarView(urlV),
+            urlV,
             AvatarPlaceholder(nameV, colorV))
 
+    private val imageView = ImageView()
+
     init {
-        ap.removeWhen { ai.imageAvailable }
         this.add(ap)
-        this.add(ai)
 
         this.minHeight = avatarSize
         this.minWidth = avatarSize
+
+        updateImage(urlV)
+    }
+
+
+    private fun updateImage(urlV: ObservableValue<String>) {
+        val url = urlV.value
+        if (url != null) {
+            val i = AvatarProvider.getAvatar(url)
+            if (i != null) {
+                imageView.imageProperty().bind(i)
+                this.children.setAll(imageView)
+            }
+        }
+        urlV.addListener { _, _, newValue ->
+            newValue ?: return@addListener
+            val imgPrp = AvatarProvider.getAvatar(newValue)
+            imgPrp ?: return@addListener
+            imageView.imageProperty().cleanBind(imgPrp)
+            this.children.setAll(imageView)
+        }
     }
 }
