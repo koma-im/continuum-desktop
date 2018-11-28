7f61845d2609a1940147d7e728a48c95051fc522
druig <14120312+druig@users.noreply.github.com>
2018-11-28 08:02:34 +0000

    delete RuntimeJsonAdapterFactory

--- src/koma/matrix/event/room_message/adapter.kt
+++ src/koma/matrix/event/room_message/adapter.kt
@@ -3,27 +3,8 @@ package koma.matrix.event.rooRoomEvent
 import com.squareup.moshi.*
 import koma.matrix.event.room_message.*
 import koma.matrix.event.room_message.state.*
-import koma.matrix.json.RuntimeJsonAdapterFactory
 import matrix.event.room_message.RoomEventType
 import java.lang.reflect.Type
-
-fun getPolyRoomEventAdapter(): JsonAdapter.Factory {
-    val factory = RuntimeJsonAdapterFactory(RoomEvent::class.java, "type", MRoomUnrecognized::class.java)
-    factory.registerSubtype(MRoomAliases::class.java, RoomEventType.Aliases.toString())
-    factory.registerSubtype(MRoomMessage::class.java, RoomEventType.Message.toString())
-    factory.registerSubtype(MRoomAvatar::class.java, RoomEventType.Avatar .toString())
-    factory.registerSubtype(MRoomCanonAlias::class.java, RoomEventType.CanonAlias .toString())
-    factory.registerSubtype(MRoomCreate::class.java, RoomEventType.Create.toString())
-    factory.registerSubtype(MRoomGuestAccess::class.java, RoomEventType.GuestAccess .toString())
-    factory.registerSubtype(MRoomHistoryVisibility::class.java, RoomEventType.HistoryVisibility.toString())
-    factory.registerSubtype(MRoomJoinRule::class.java, RoomEventType.JoinRule   .toString())
-    factory.registerSubtype(MRoomMember::class.java, RoomEventType.Member.toString())
-    factory.registerSubtype(MRoomName::class.java, RoomEventType.Name   .toString())
-    factory.registerSubtype(MRoomPowerLevels::class.java, RoomEventType.PowerLevels.toString())
-    factory.registerSubtype(MRoomRedaction::class.java, RoomEventType.Redaction   .toString())
-    factory.registerSubtype(MRoomTopic::class.java, RoomEventType.Topic .toString())
-    return factory
-}
 
 class RoomEventAdapterFactory: JsonAdapter.Factory {
     override fun create(type: Type, annotations: MutableSet<out Annotation>, moshi: Moshi): JsonAdapter<RoomEvent>? {
