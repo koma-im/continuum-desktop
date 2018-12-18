6f62864c065ce75567b0c247ccf826a240f0581b
rinfuyu <36916353+rinfuyu@users.noreply.github.com>
2018-12-18 14:11:38 +0900

    start syncing

--- src/koma/controller/ChatController.kt
+++ src/koma/controller/ChatController.kt
@@ -1,41 +1,46 @@
 package controller
 
+import com.github.kittinunf.result.Result
 import koma.controller.events_processing.processEventsResult
-import koma.controller.sync.startSyncing
+import koma.controller.sync.MatrixSyncReceiver
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.javafx.JavaFx
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import matrix.ApiClient
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 /**
  * Created by developer on 2017/6/22.
  */
 class ChatController(
         val apiClient: ApiClient) {
-
-    private val shutdownSignalChan = Channel<Unit>()
-
+    private val sync = MatrixSyncReceiver()
     init{
 
     }
 
     fun start() {
         val start = if (apiClient.profile.hasRooms) apiClient.next_batch else null
-        val syncEventChannel = startSyncing(start, shutdownSignalChan)
+        sync.since = start
         GlobalScope.launch(Dispatchers.JavaFx) {
-            for (s in syncEventChannel) {
-                apiClient.profile.processEventsResult(s)
+            for (s in sync.events) {
+                if (s is Result.Success) {
+                    apiClient.profile.processEventsResult(s.value)
+                } else {
+                    logger.warn { "sync stopped because of $s" }
+                }
             }
         }
+        sync.startSyncing()
     }
 
     fun shutdown() {
         runBlocking {
-            shutdownSignalChan.send(Unit)
-            shutdownSignalChan.receive()
+            sync.stopSyncing()
         }
     }
 }
