20cf9c2f4fce8697128584981c38eec85b3eec48
rinfuyu <36916353+rinfuyu@users.noreply.github.com>
2018-12-24 14:34:34 +0900

    modularize

--- src/koma/koma_app/error_handler.kt
+++ src/koma/koma_app/error_handler.kt
@@ -1,13 +1,12 @@
 package koma.koma_app
 
-import java.util.logging.Level
-import java.util.logging.Logger
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 class NoAlertErrorHandler: Thread.UncaughtExceptionHandler {
-    val log = Logger.getLogger("ErrorHandler")
-
-    override fun uncaughtException(t: Thread?, e: Throwable?) {
-        log.log(Level.SEVERE, "Uncaught error", e)
-        e?.printStackTrace()
+        override fun uncaughtException(t: Thread?, e: Throwable?) {
+        logger.error { "Uncaught error $e" }
+        logger.warn { "Uncaught error stacktrace ${e?.stackTrace}" }
     }
 }
