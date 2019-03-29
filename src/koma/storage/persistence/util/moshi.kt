a2b9687dd2c4a6e1ac78c8c828e7b12ca53a48ea
yuforia <uforia@continuum.link>
2019-03-29 12:46:40 +0900

    clean unused

--- src/koma/storage/persistence/util/moshi.kt
+++
@@ -1,8 +0,0 @@
-package koma.storage.persistence.util
-
-import koma.matrix.json.MoshiInstance
-import koma.storage.persistence.settings.encoding.ProxyAdapter
-
-object AppMoshi {
-    val moshi = MoshiInstance.moshiBuilder.add(ProxyAdapter()).build()
-}
