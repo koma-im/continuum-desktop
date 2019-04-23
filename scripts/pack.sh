5c1d6d9c38a22210b7c9ed187b0f13c01a21072a
yuforia <uforia@continuum.link>
2019-04-23 13:16:51 +0900

    name

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -2,5 +2,5 @@
 set -ev
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
   ./gradlew jar
-  mv build/libs/*-without-dependencies.jar ./deploy/
+  mv build/libs/*-without-dependencies.jar ./deploy/$APPNAME-$TRAVIS_TAG-without-dependencies.jar
 fi
