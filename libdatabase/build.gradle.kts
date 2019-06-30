59be489875c9898dd5a15a815de3a5e2a9269fab
yuforia <uforia@continuum.link>
2019-06-30 13:42:30 +0900

    configure multi-project dependencies

--- libdatabase/build.gradle.kts
+++ libdatabase/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.kotlin.kapt")
 }
 
@@ -7,20 +6,14 @@ repositories {
     jcenter()
     mavenLocal()
     mavenCentral()
-    maven("https://jitpack.io")
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.requery:requery-kotlin:1.6.0")
     implementation("io.requery:requery:1.6.0")
     implementation("com.h2database:h2:1.4.198")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
-    implementation("io.github.microutils:kotlin-logging:1.6.22")
-    implementation("io.github.koma-im:koma-library:0.7.15")
+
     compile(project(":libutil"))
     kapt("io.requery:requery-processor:1.5.1")
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
