21d02a8b569c4840d7bb5e46b37737ee22dc62c6
yuforia <uforia@continuum.link>
2019-08-28 21:15:18 +0900

    build AppImage

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-  ./gradlew jar
-  mv build/libs/*-without-dependencies.jar ./deploy/$APPNAME-$TRAVIS_TAG-without-dependencies.jar
+  wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+  chmod +x linuxdeploy-x86_64.AppImage
+  gradle runtime
+  mkdir -p build/image/usr/bin
+  cd build/image/usr/bin
+  ln -s ../../bin/* .
+  cd -
+  env VERSION=$TRAVIS_TAG linuxdeploy-x86_64.AppImage \
+    --appdir build/image \
+    -i src/main/resources/icon/koma.png \
+    -d scripts/continuum.desktop \
+    --output appimage
+  mv *.AppImage ./deploy/$APPNAME-$TRAVIS_TAG-linux.AppImage
 fi
+
