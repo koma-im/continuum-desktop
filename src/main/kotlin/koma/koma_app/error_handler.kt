b807369f8e411c9285350acb64f2c751871eb809
yuforia <uforia@continuum.link>
2019-10-16 16:19:58 +0900

    shutdown when frozen

--- src/main/kotlin/koma/koma_app/error_handler.kt
+++ src/main/kotlin/koma/koma_app/error_handler.kt
@@ -2,28 +2,30 @@ package koma.koma_app
 
 import mu.KotlinLogging
 import java.io.PrintStream
-
+import java.util.concurrent.ConcurrentLinkedDeque
 
 
 private val logger = KotlinLogging.logger {}
 
 class NoAlertErrorHandler: Thread.UncaughtExceptionHandler {
-        override fun uncaughtException(t: Thread?, e: Throwable?) {
-            logger.error { "Uncaught error $e" }
-            e?.let { printStackTrace(e) }
-
+    private val recent = ConcurrentLinkedDeque<Pair<Long, Throwable?>>()
+    override fun uncaughtException(t: Thread?, e: Throwable?) {
+        try {
+            recent.add(System.nanoTime() to e)
+            logger.error { "uncaught error $e" }
+            e?.printStackTrace()
+            checkAmount()
+        } catch (e: Exception) {
+            logger.error { "got error handling uncaught exception: $e" }
+        }
     }
-}
-
-private fun printStackTrace(e: Throwable, s: PrintStream = System.err) {
-
-    for (stackTraceElement in e.stackTrace) {
-        s.println("\tat $stackTraceElement")
-    }
-    for (throwable in e.suppressed) {
-        s.println("Suppressed: \t $throwable")
-    }
-    e.cause?.let {
-        s.println("Cause: \t ${e.cause}")
+    private fun checkAmount() {
+        val now = System.nanoTime()
+        val n = recent.descendingIterator().asSequence().takeWhile { (now - it.first) < 1e9 }.count()
+        if (n > 9) {
+            logger.error { "$n errors in 1s, shutting down" }
+            Runtime.getRuntime().exit(5)
+        }
+        recent.removeIf { (now - it.first) > 1e9 }
     }
 }
