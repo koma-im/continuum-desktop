03fac037328901f73396c66ddd9c5acff801cffc
yuforia <uforia@continuum.link>
2020-02-13 17:00:29 +0900

    reuse downloaded binary locally

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-version = "0.9.37"
+version = "0.9.38"
 
 plugins {
     kotlin("jvm") version "1.3.61"
--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set -ev
+
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
   mkdir -p bin
   cd bin
-  wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+  if [ ! -f linuxdeploy-x86_64.AppImage ]; then
+    wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+  fi
   chmod +x linuxdeploy-x86_64.AppImage
   cd ..
   gradle runtime
   mkdir -p build/image/usr/bin
   cd build/image/usr/bin
-  ln -s ../../bin/* .
+  ln -sb ../../bin/* .
   cd -
-  env VERSION=$TRAVIS_TAG ./bin/linuxdeploy-x86_64.AppImage \
+  env VERSION="$TRAVIS_TAG" ./bin/linuxdeploy-x86_64.AppImage \
     --appdir build/image \
     -i src/main/resources/icon/koma.png \
     -d scripts/continuum.desktop \
     --output appimage
-  mv Continuum*.AppImage ./deploy/$APPNAME-$TRAVIS_TAG-linux.AppImage
+  mv Continuum*.AppImage ./deploy/"$APPNAME"-"$TRAVIS_TAG"-linux.AppImage
 fi
 
